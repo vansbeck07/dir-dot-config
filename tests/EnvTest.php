03a26989aa0e4b18014266f0991a81c6c57eddfd
Evans Owusu Ofori <vansbeck07@gmail.com>
2026-03-26 18:01:58 +0000

    First Commit

---
+++ tests/EnvTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Vansbeck07\Arr;
+use Vansbeck07\Config;
+
+class EnvTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $configArray = (new Config(__DIR__.'/../config/'))->get();
+
+        $this->assertEquals('production', Arr::multiKeyGet('app.app_env', $configArray));
+        $this->assertEquals('test', Arr::multiKeyGet('database.default.dbname', $configArray));
+
+        $updated = Arr::multiKeySet('app.app_env', 'development', $configArray);
+        $this->assertEquals('development', Arr::multiKeyGet('app.app_env', $updated));
+    }
+}
